c6d57611d2d6bf35d5ca46af0a0bb0607ed51cb1
king-p1 <oladelex4@gmail.com>
2025-05-01 20:18:22 -0400

    pload

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import { BoardList } from './_components/board/board-list'
 const Dashboard = ({searchParams}:DashboardProps) => {
   const { organization } = useOrganization()
 
-  const query = React.use(searchParams)
+  // const query = React.use(searchParams)
 
   return (
     <div className='flex-1 h-[calc(100%-80px)] p-3 -mt-3 '>
@@ -17,7 +17,8 @@ const Dashboard = ({searchParams}:DashboardProps) => {
 ):(
   <BoardList
   OrgId={organization.id}
-  query={query}
+  // query={query}
+  query={searchParams}
   />
 )}
     </div>
